a28da259fbc1b8be3424b586de7288371088220e
赵庆明 <20995658@qq.com>
2021-12-09 20:26:00 +0800

    add code

---
+++ 课程源码/WebApi012/WebApi012/Controllers/Values1Controller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi012.Models;
+
+namespace WebApi012.Controllers
+{
+    //[Route("/api/[controller]/abc/")]
+    [Route("api/v1/Values/abc")]
+    //[Route("api/v2/Values/abc")]
+    [ApiController]
+    public class Values1Controller : ControllerBase
+    {
+        //[Route("a1/b1/c1/")]
+        //[Route("a/b/c/")]
+        [Route("/123/[Action]/456/")]
+        [HttpGet]
+        public Values Test()
+        {
+            return new Values() { Age = 20, Gender = "男", Id = 1, Name = "张三" };
+        }
+    }
+}
---
+++ 课程源码/WebApi012/WebApi012/Models/Values.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi012.Models
+{
+    public class Values
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Gender { get; set; }
+    }
+}
