d1f4a36a580226fe1b056ee2a6e7cfaa777ef4f7
赵庆明 <20995658@qq.com>
2022-01-08 19:19:32 +0800

    add code

---
+++ 课程源码/WebApi013/WebApi13/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi13.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi13.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpGet("{id1}abc{id2}")]
+        public string Get1(string id1)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost("{value1}")]
+        public void Post([FromBody] string value,[FromRoute]string value1,[FromHeader]string value2)
+        {
+        }
+
+        [HttpPost]
+        public void Post([FromBody] ValuesModel value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [NonAction]
+        public void Test()
+        {
+
+        }
+
+    }
+}
---
+++ 课程源码/WebApi013/WebApi13/Models/ValuesModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi13.Models
+{
+    public class ValuesModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string?  Gender { get; set; }
+    }
+}
