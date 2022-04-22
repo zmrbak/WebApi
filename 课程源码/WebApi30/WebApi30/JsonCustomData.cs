2578e0e4e69392f748a9cafb8ef67a6c345a6ee2
Zmrbak <20995658@qq.com>
2022-04-22 19:56:31 +0800

    add code

---
+++ 课程源码/WebApi30/WebApi30/JsonCustomData.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+internal class JsonCustomData : JsonConverter<DateTime>
+{
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return reader.GetDateTime();
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        //writer.WriteStringValue(value.ToString());
+        //writer.WriteStringValue(value);
+        writer.WriteStringValue(value.ToString("yyyy-MM-dd HH:mm:ss"));
+    }
+}
+internal class JsonCustomDataString : JsonConverter<string>
+{
+    public override string? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return reader.GetString();
+    }
+
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToUpper());
+    }
+}
