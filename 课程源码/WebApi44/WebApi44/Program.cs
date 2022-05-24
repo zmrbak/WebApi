b7df253acdb0985d6c41efe1395747b0319b5047
Zmrbak <20995658@qq.com>
2022-05-24 19:28:32 +0800

    add code

---
+++ 课程源码/WebApi44/WebApi44/Program.cs
@@ -0,0 +1,78 @@
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+JwtSecurityTokenHandler JwtTokenHandler = new JwtSecurityTokenHandler();
+SymmetricSecurityKey SecurityKey = new SymmetricSecurityKey(Guid.NewGuid().ToByteArray());
+string GenerateJwtToken(string name)
+{
+    if (string.IsNullOrEmpty(name))
+    {
+        throw new InvalidOperationException("Name is not specified.");
+    }
+
+    var claims = new[] { new Claim(ClaimTypes.Name, name) };
+    var credentials = new SigningCredentials(SecurityKey, SecurityAlgorithms.HmacSha256);
+    var token = new JwtSecurityToken(
+        "ExampleServer", 
+        "ExampleClients", 
+        claims, 
+        expires: DateTime.Now.AddSeconds(60), 
+        signingCredentials: credentials);
+    return JwtTokenHandler.WriteToken(token);
+}
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
+    {
+        policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+        policy.RequireClaim(ClaimTypes.Name);
+    });
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateActor = false,
+                ValidateLifetime = true,
+                IssuerSigningKey = SecurityKey
+            };
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/generateJwtToken", context =>
+{
+    return context.Response.WriteAsync(GenerateJwtToken(context.Request.Query["name"]));
+});
+
+app.Run();
